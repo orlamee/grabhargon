6ddb013ba64d7ce471c8242f723d5873145b862b
orlamee <meetmichaelakindele@gmail.com>
2023-06-05 10:27:53 +0100

    carousel

--- src/components/testimony.jsx
+++ src/components/testimony.jsx
@@ -1,5 +1,7 @@
 
 import whatusers from "../assets/whatusers.svg";
+import props from "../assets/test.svg";
+import q from "../assets/q.svg";
 
 export default function Testimony() {
   return (
@@ -8,6 +10,23 @@ export default function Testimony() {
         <div className="row justify-content-center">
           <div className="col-sm-6 text-center">
             <img src={whatusers} alt="What Our Users Say" className="img-fluid" />
+          </div>
+        </div>
+        <div className="row mt-9 justify-content-between">
+          <div className="col-sm-5 mb-3">
+            <div className="test-card">
+              <div className="row justify-content-center">
+                <div className="col-sm-8">
+                  <div className="text-center">
+                    <img src={q} alt="q" className="img-fluid mb-3"/>
+                    <h6>“Hargon has transformed my finances through quick and accessible loans”</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <img src={props} alt="testimonial" className="img-fluid w-100" />
           </div>
         </div>
        
