cf0ef9509a94f4d8dad7bf3a76fd46b03ba8b8b2
orlamee <meetmichaelakindele@gmail.com>
2023-06-30 12:08:56 +0100

    fixes

--- src/components/bills/billPayment.jsx
+++ src/components/bills/billPayment.jsx
@@ -9,7 +9,7 @@ export default function BillPayment () {
         <div className="row justify-content-between">
           <div className="col-sm-5 mb-3 mt-5">
             <h2 className="mb-5 text-white">Never miss a bill payment<br/>again</h2>
-            <h6 className="text-white" style={{fontSize: "14px", lineHeight: "20px"}}>With Hargon, you can say goodbye to missed bill payments. Enjoy settling all your bills on time and seamlessly using our automated bill payments system, including auto-debits from Hargon wallets and other credit options.</h6>
+            <h6 className="text-white w-87" style={{fontSize: "14px", lineHeight: "20px"}}>With Hargon, you can say goodbye to missed bill payments. Enjoy settling all your bills on time and seamlessly using our automated bill payments system, including auto-debits from Hargon wallets and other credit options.</h6>
           </div>
           <div className="col-sm-6">
             <img src={bills} alt="bill payment" className="img-fluid w-100" />
