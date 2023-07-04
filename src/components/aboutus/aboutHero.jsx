14e671c64a1900b7e626ac444cc2c34494cb5f13
orlamee <meetmichaelakindele@gmail.com>
2023-07-04 16:33:21 +0100

    fixes

--- src/components/aboutus/aboutHero.jsx
+++ src/components/aboutus/aboutHero.jsx
@@ -1,5 +1,6 @@
 import phone from "../../assets/aboutimg.png";
 import down from "../../assets/downn.svg";
+import welcome from "../../assets/welcome.svg";
 
 
 export default function AboutHero () {
@@ -8,7 +9,7 @@ export default function AboutHero () {
       <div className='container'>
         <div className="row justify-content-between">
           <div className="col-sm-5 mb-3 mt-5">
-            <h6>Welcome to Hargon, Nigeria's multi-faceted credit platform. Hargon is committed to leveraging technology to provide innovative financial solutions and empower individuals and businesses nationwide.</h6>
+            <img src={welcome} alt="wwelcome to hargon" className="img-fluid" />
             <img src={down} alt="about us" className="img-fluid mt-5" />
           </div>
           <div className="col-sm-5">
