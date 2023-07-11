afe44f4cbf47d2f00b6016b076a94dd25bf3a835
orlamee <meetmichaelakindele@gmail.com>
2023-07-11 13:33:04 +0100

    fixes

--- src/components/personalPage/educationLoan.jsx
+++ src/components/personalPage/educationLoan.jsx
@@ -1,16 +1,21 @@
 
-import shape from "../../assets/shape.svg";
+import shape from "../../assets/shapeyour.png";
+import right from "../../assets/rightpartner.svg";
+import down from "../../assets/fundings.svg";
 
 
 export default function EducationalLoan () {
   return (
     <section className='educational'>
       <div className="container">
-       <div className="row justify-content-center">
-        <div className="col-sm-8 text-center">
-          <span className="ed">Educational loans</span>
-          <img src={shape} alt="Educational loans" className="img-fluid my-5" />
-          <p className="text-white">From undergraduate to postgraduate, we are here to ensure<br/>you power through your educational journey without the<br/>limitation of funds.</p>
+       <div className="row justify-content-between">
+        <div className="col-sm-6">
+          <img src={shape} alt="Educational loans" className="img-fluid w-100" />
+        </div>
+        <div className="col-sm-5 mt-3">
+          <img src={right} alt="Shape your future with the right partner" className="img-fluid" />
+          <p className="my-5 w-75 p-bus">From undergraduate to postgraduate, we are here to ensure you power through your educational journey without the limitation of funds.</p>
+          <img src={down} alt="Shape your future with the right partner" className="img-fluid i-bus" />
         </div>
        </div>
       </div>
