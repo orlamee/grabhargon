8607f3270bc7f181caaf4e6e64d5f3a1aaab6f6a
orlamee <meetmichaelakindele@gmail.com>
2023-07-13 12:55:44 +0100

    fixes

--- src/components/apply/contentApply.jsx
+++ src/components/apply/contentApply.jsx
@@ -17,7 +17,7 @@ export default function ContentApply () {
         <div className="row mt-9">
           <div className="col">
             <Form>
-              <Row className="mb-4">
+              <Row className="mb-5">
                 <Col>
                   <Form.Label>First name <span className="text-danger">*</span></Form.Label>
                   <Form.Control placeholder="Enter first name" />
@@ -27,7 +27,7 @@ export default function ContentApply () {
                   <Form.Control placeholder="Enter last name" />
                 </Col>
               </Row>
-              <Row className="mb-4">
+              <Row className="mb-5">
                 <Col>
                   <Form.Label>Email <span className="text-danger">*</span></Form.Label>
                   <Form.Control type="email" placeholder="Enter email address" />
@@ -37,13 +37,39 @@ export default function ContentApply () {
                   <Form.Control type="tel" placeholder="Enter phone number" />
                 </Col>
               </Row>
-              <Form.Group className="mb-4">
+              <Form.Group className="mb-5">
                 <Form.Label>Website/portfolio link</Form.Label>
                 <Form.Control type="url" placeholder="Website/portfolio link" />
               </Form.Group>
-              <Form.Group className="mb-4">
+              <Form.Group className="mb-5">
                 <Form.Label>Linkedin Profile</Form.Label>
                 <Form.Control type="url" placeholder="Linkedin profile link" />
+              </Form.Group>
+              <Form.Group className="mb-5">
+                <div><Form.Label>Work type <span className="text-danger">*</span></Form.Label></div>
+                <div class="form-check form-check-inline me-5">
+                  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
+                  <label class="form-check-label" for="inlineRadio1">On-site</label>
+                </div>
+                <div class="form-check form-check-inline me-5">
+                  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+                  <label class="form-check-label" for="inlineRadio2">Hybrid</label>
+                </div>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+                  <label class="form-check-label" for="inlineRadio2">Remote</label>
+                </div>
+              </Form.Group>
+              <Form.Group className="mb-5">
+                <div><Form.Label>Available to Start Immediately <span className="text-danger">*</span></Form.Label></div>
+                <div class="form-check form-check-inline me-5">
+                  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
+                  <label class="form-check-label" for="inlineRadio1">Yes</label>
+                </div>
+                <div class="form-check form-check-inline me-5">
+                  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+                  <label class="form-check-label" for="inlineRadio2">No</label>
+                </div>
               </Form.Group>
             </Form>
           </div>
