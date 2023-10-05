457e3e9ec5de3698e3e725d84f0b956457960a78
remilekun-hub <atandaremilekun@gmail.com>
2023-10-05 16:32:30 +0100

    added subdomain

--- src/components/businessPage/businessHeader.jsx
+++ src/components/businessPage/businessHeader.jsx
@@ -1,67 +1,81 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import logo from "../../assets/hargon000.svg";
 import globe from "../../assets/globe.svg";
-import { Link } from 'react-router-dom';
-import DropDownFeatures from './dropDownFeatures';
-import DropDownCompany from '../dropdownCompany';
+import { Link } from "react-router-dom";
+import DropDownFeatures from "./dropDownFeatures";
+import DropDownCompany from "../dropdownCompany";
 import { useNavigate } from "react-router-dom";
 
-
-function BusinessHeader () {
+function BusinessHeader() {
   const navigate = useNavigate();
   return (
-    <Navbar bg="white" expand="lg" className='py-3 header sticky-top'>
+    <Navbar bg="white" expand="lg" className="py-3 header sticky-top">
       <Container>
-        <Navbar.Brand href="/"><img src={logo} alt="stackivy" className='img-fluid' /></Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img src={logo} alt="stackivy" className="img-fluid" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto p-b">
             <li className="nav-item">
-              <a className='nav-link' href='/personal'>
+              <a className="nav-link" href="/personal">
                 Personal
               </a>
             </li>
             <li className="nav-item">
-              <a className='nav-link ms-0' href='/business'>
+              <a className="nav-link ms-0" href="/business">
                 Business
               </a>
             </li>
           </Nav>
           <Nav className="mx-auto">
             <li className="nav-item dropdown">
-              <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <Link
+                className="nav-link dropdown-toggle"
+                to="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 Features
               </Link>
-              <DropDownFeatures/>
+              <DropDownFeatures />
             </li>
-            <Nav.Link href="/">Partnership <span className='p-cs bg-black'>Coming Soon</span></Nav.Link>
+            <Nav.Link href="/">
+              Partnership <span className="p-cs bg-black">Coming Soon</span>
+            </Nav.Link>
             <Nav.Link href="/auction">Auction</Nav.Link>
             <li className="nav-item dropdown">
-              <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <Link
+                className="nav-link dropdown-toggle"
+                to="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 Company
               </Link>
-              <DropDownCompany/>
+              <DropDownCompany />
             </li>
           </Nav>
           <Nav className="ms-auto">
-            <Nav.Link href="/"><img src={globe} alt="stackivy" className='img-fluid globe' /></Nav.Link>
-            <Nav.Link>
-              <button
-                className="btn btn-primary btn-main bg-black text-white rounded-pill px-5 py-3"
-                onClick={() => navigate("/sign-up")}
-              >
-                Sign in
-              </button>
+            <Nav.Link href="/">
+              <img src={globe} alt="stackivy" className="img-fluid globe" />
             </Nav.Link>
+
+            <a
+              href="https://business.hargon.africa/sign-in"
+              className="btn btn-primary btn-main bg-black text-white rounded-pill px-5 py-3"
+            >
+              Sign in
+            </a>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    
-  )
+  );
 }
 
-
-export default BusinessHeader
+export default BusinessHeader;
